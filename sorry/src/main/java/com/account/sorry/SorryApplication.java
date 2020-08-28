74a579c26fb1c58809e760feeb4eb57a65474106
sorryisme <wivipp39@gmail.com>
2020-08-28 22:06:44 +0900

    20200830

---
+++ sorry/src/main/java/com/account/sorry/SorryApplication.java
@@ -0,0 +1,14 @@
+package com.account.sorry;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorryApplication.class, args);
+	}
+
+}
---
+++ sorry/src/test/java/com/account/sorry/SorryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.sorry;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
