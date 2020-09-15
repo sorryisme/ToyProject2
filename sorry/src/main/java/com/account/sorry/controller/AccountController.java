d54c51e975df2ecc1d33d79f02936076e357c171
sorryisme <wivipp39@gmail.com>
2020-09-15 20:40:00 +0900

    20200915 Controller 작성

--- sorry/src/main/java/com/account/sorry/controller/AccountController.java
+++ sorry/src/main/java/com/account/sorry/controller/AccountController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class AccountController {
 
-   /* @PostMapping("/account/save")
-    @ResponseBody
-    public Map<>*/
+   @PostMapping("/account/save")
+   @ResponseBody
+   public Map<String,Object> saveAccount(){
+       return new HashMap<String, Object>();
+   }
 }
